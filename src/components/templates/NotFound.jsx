207f4425d6e56104bf4a2433ee3fd79b84a2f976
Rishabh Dubey <rishabhdubey006@gmail.com>
2024-09-15 15:04:39 +0530

    done with all major compornts

---
+++ src/components/templates/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NOT_FOUND } from '../../utils/constents'
+const NotFound = () => {
+  return (
+    <div>
+      <img className='w-screen  h-full object-cover' src={NOT_FOUND}/>
+        
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/components/templates/PlayTrailer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactPlayer from "react-player/youtube";
+import { useSelector } from "react-redux";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
+
+
+const PlayTrailer = () => {
+  const { pathname } = useLocation();
+  const Navigate = useNavigate();
+  const category = pathname.includes("movie") ? "movie" : "tv";
+  const ytvidoes = useSelector((state) => state[category].info.videos);
+  console.log(ytvidoes);
+  return  (
+    <div className="absolute bg-[rgba(0,0,0,0.5)] z-[100]  h-screen w-screen top-0 left-0  flex bg-  items-center justify-center">
+      <Link
+        onClick={() => Navigate(-1)}
+        className="absolute selection:hover:text-purple-300 ri-close-fill text-3xl text-white right-[15%] top-[5%] "
+      ></Link>
+     { ytvidoes? (<ReactPlayer
+        height={600}
+        width={900}
+        url={`https://www.youtube.com/watch?v=${ytvidoes.key}`}
+      />):<NotFound/>}
+    </div>
+  )
+};
+
+export default PlayTrailer;
